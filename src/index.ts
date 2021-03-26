1fd89d8fddc366f917255a1b284016d286c4f2f5
Nigar Movsumova <movsum.nigar@gmail.com>
2021-03-27 02:19:37 +0400

    added a test custom command, installed simple-git, removed comments

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,29 @@
-import {Command, flags} from '@oclif/command'
+import { Command, flags } from '@oclif/command'
+import simpleGit, { SimpleGit } from 'simple-git';
+const git: SimpleGit = simpleGit();
 
 class Saj extends Command {
   static description = 'describe the command here'
 
   static flags = {
-    // add --version flag to show CLI version
-    version: flags.version({char: 'v'}),
-    help: flags.help({char: 'h'}),
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
-    // flag with no value (-f, --force)
-    force: flags.boolean({char: 'f'}),
+    version: flags.version({ char: 'v' }),
+    help: flags.help({ char: 'h' }),
+    name: flags.string({ char: 'n', description: 'name to print' }),
+    force: flags.boolean({ char: 'f' }),
+    custom: flags.string({ char: 'c', description: 'test flag' }),
   }
 
-  static args = [{name: 'file'}]
+  static args = [{ name: 'file' }]
 
   async run() {
-    const {args, flags} = this.parse(Saj)
-
+    const { args, flags } = this.parse(Saj)
     const name = flags.name ?? 'world'
-    this.log(`hello ${name} from ./src/index.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
+    const custom = flags.custom
+
+    if (flags.custom) {
+      this.log(`custom input ${custom}`)
+      git.checkIsRepo()
+        .then(isRepo => console.log(isRepo));
     }
   }
 }
